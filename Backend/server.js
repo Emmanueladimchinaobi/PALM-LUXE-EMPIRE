d864abdcd5a73d3209bbf9f08d1b6e2372a83021
Emmanueladimchinaobi <deborahamybliss@gmail.com>
2026-07-24 03:31:11 +0100

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://palm-luxe-empire.vercel.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/orders", orderRoutes);
